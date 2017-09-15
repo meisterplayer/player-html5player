7065bcc9a85f13406532345507e2da8e251d91b2
Thomas van Dam <t.vandam@wearetriple.com>
2017-09-15 16:39:35 +0200

    Remove pointless failing test

--- test/Html5Player.test.js
+++ test/Html5Player.test.js
@@ -13,11 +13,3 @@ describe('Html5Player class', () => {
         expect(Html5Player.pluginVersion).toMatch(/\d+\.\d+\.\d+/);
     });
 });
-
-describe('The rest of the test suite', () => {
-    test('It should be written', () => {
-        const test = { testsWritten: false };
-
-        expect(test).toEqual({ testsWritten: true });
-    });
-});
